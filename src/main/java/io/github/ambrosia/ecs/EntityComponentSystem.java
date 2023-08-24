5223caa41577457606a9d8f6d7519c47e9e16192
Poisoned-Honey <poisonedhoney13@gmail.com>
2023-08-24 10:36:36 -0700

    Add a little docs

--- src/main/java/io/github/ambrosia/ecs/EntityComponentSystem.java
+++ src/main/java/io/github/ambrosia/ecs/EntityComponentSystem.java
@@ -5,7 +5,15 @@ import io.github.ambrosia.ecs.query.Query;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * The central engine of the ECS. There should only be one instance of this
+ * per "game". There is no built-in functionality for e.g. running appls, 
+ * so game engines will need to wrap around and work with the ECS.
+ */
 public class EntityComponentSystem {
+	/**
+	 * A component with a reference to which entity it belongs to.
+	 */
 	public record AttachedComponent(Component component, int index) {}
 
 	private final Resources resources;
@@ -17,8 +25,10 @@ public class EntityComponentSystem {
 	// ArrayList might not be the best data structure for this
 	// TODO: maybe a HashMap keyed with component type, with values of lists of components?
 	private final ArrayList<AttachedComponent> components;
-
-
+	
+	/**
+	 * Creates a new Entity-Component-System ready for use.
+	 */
 	public EntityComponentSystem() {
 		this.entities = new Entities();
 		this.components = new ArrayList<>();
@@ -29,20 +39,30 @@ public class EntityComponentSystem {
 	}
 
 	/**
-	 * Returns an object that is able to query many things about the ECS, like its components.
+	 * Returns an object that is able to query many things about the components and entities of this ECS.
 	 */
 	public Query query() {
 		return this.query;
 	}
-
+	
+	/**
+	 * Returns an object that handles the systems of this ECS. With this object, you can add and run
+	 * systems.
+	 */
 	public Systems systems() {
 		return this.systems;
 	}
-
+	
+	/**
+	 * Returns an object that handles the resources of this ECS. With it, resources can be initialized, added, and fetched.
+	 */
 	public Resources resources() {
 		return this.resources;
 	}
 	
+	/** 
+	 * Returns an object that handles event sending and receiving in this ECS, allowing communication between systems.
+	 * */
 	public Events events() {
 		return this.events;
 	}
